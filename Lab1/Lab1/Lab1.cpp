a7c064d49f1d2d8e6b7730bc6fe4a06eeb54dd99
IlyhaDmi28 <95546576+IlyhaDmi28@users.noreply.github.com>
2023-02-05 14:50:15 +0300

    Add files via upload

---
+++ Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+void HanoiTower(int i, int k, int n, int* count) {
+
+	if (n == 1) {
+
+		cout << (*count)++ << ") Перместить диск 1 с стержня " << i << " на стержень " << k << ".\n";
+
+	}
+	else {
+
+		int tmp = n;
+
+		HanoiTower(i, tmp, n - 1, count);
+
+		cout << (*count)++ << ") Переместить диск " << n << " с стержня " << i << " на стержень " << k << ".\n";
+
+		HanoiTower(tmp, k, n - 1, count);
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int count = 1;
+	int pinc, diskc;
+
+	cout << "Введите количество стержней: "; cin >> pinc;
+	cout << "Введите количество дисков: "; cin >> diskc;
+	if (pinc <= 0 || diskc <= 0) { cout << "Неверные данные!!!"; return 0; };
+	HanoiTower(1, pinc, diskc, &count);
+
+	return 0;
+}
